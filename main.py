00332adb2b6fce90f9fdfcadf4c75d57ae103dfe
jhessler10 <jacobahessler@gmail.com>
2021-04-02 10:05:44 -0400

    Imported CSV File, and Basic File Cleaning

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 # Movie Recommender Program 
 # CNIT 481 Group Project
 #Jacob Hessler, Jitesh Motati
-
+import pandas as pd
 
 # import data here...
-
+url = 'https://raw.githubusercontent.com/jmotati/MovieRecommender/main/movies_metadata.csv'
+df = pd.read_csv(url, error_bad_lines=False)
 
 # clean data
+clean_df = df[['title', 'overview', 'vote_average', 'vote_count']]
+print(clean_df)
 
 # train data
 
