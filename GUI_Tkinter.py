cff14fe99d63ec713ee8cbe68c0fbaad02a1da8a
jhessler10 <jacobahessler@gmail.com>
2021-04-20 20:06:09 -0400

    Added Multiple Filter Fields

--- GUI_Tkinter.py
+++ GUI_Tkinter.py
@@ -6,8 +6,8 @@ class Application:
 
 
         #display main frame          
-        frame = Frame(master)
-        frame.pack()
+        #frame = Frame(master)
+        #frame.pack()
         
         #display Menu
         menu = Menu(master)
@@ -20,24 +20,37 @@ class Application:
             text = 'Movie Reccommendation',
             font = ('Bebas Neue', 18),
             justify="left")
-        title_label.pack(pady=20)
-
+        title_label.grid(column=0, row=0)
         #Textboxes
-        txt_lbl1 = Label(master,text = 'Name',font = ('Times New Roman', 12))
-        txt_lbl1.pack()
+        txt_lbl1 = Label(master,text = 'Movie Title',font = ('Times New Roman', 12))
+        txt_lbl1.grid(column=0, row=1)
         
-        txt_entry = Entry(master,width=10)
-        txt_entry.pack() #TO DO - Figure out how to add input to csv dataset.
+        txt_entry1 = Entry(master,width=10)
+        txt_entry1.grid(column=1, row=1) 
         
+        txt_lbl2 = Label(master,text = 'Movie Genre',font = ('Times New Roman', 12))
+        txt_lbl2.grid(column=0, row=2)
+        
+        txt_entry2 = Entry(master,width=10)
+        txt_entry2.grid(column=1, row=2)
+
+        txt_lbl3 = Label(master,text = 'Movie Rating',font = ('Times New Roman', 12))
+        txt_lbl3.grid(column=0, row=3)
+        
+        txt_entry3 = Entry(master,width=10)
+        txt_entry3.grid(column=1, row=3)  
+
+        #TO DO - Figure out how to add input to csv dataset.
+
         #display Movie Dataset
         lbl = Label(master,
             text = 'Display Movie Dataset Here',
             font = ('Times New Roman', 12))
-        lbl.pack()
+        lbl.grid(column=0, row=4)
 
         #display buttons
         btn_one = Button(master, text='Exit', command=quit)
-        btn_one.pack(side=BOTTOM)
+        btn_one.grid(column=0, row=5)
 
 # GUI configurations
 root = Tk()                
